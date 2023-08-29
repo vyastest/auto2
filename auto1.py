e271042103c00122005a839e6a4c06e4866a02a2
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-29 17:46:51 +0200

    Update auto1.py

--- auto1.py
+++ auto1.py
@@ -1,22 +1,21 @@
 import streamlit as st
 
-# Sample list of preset options
-preset_options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']
+# Sample list of suggestions
+suggestions = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew']
 
 def main():
-    st.title("Text Input with Autocomplete and Preset Options")
+    st.title("Text Input with Autocomplete Dropdown")
 
-    # Text input for custom input with autocomplete
-    custom_input = st.text_input("Enter your own option:", "", key="custom_input")
+    # Text input for custom input
+    custom_input = st.text_input("Enter a fruit:", "")
 
-    # Filter preset options based on user input
-    filtered_options = [option for option in preset_options if custom_input.lower() in option.lower()]
+    # Filter suggestions based on user input
+    filtered_suggestions = [sugg for sugg in suggestions if custom_input.lower() in sugg.lower()]
 
-    # Dropdown for preset options
-    selected_option = st.selectbox("Select a preset option:", filtered_options)
-
-    st.write("Custom Input:", custom_input)
-    st.write("Selected Preset Option:", selected_option)
+    # Display suggestions as a dropdown
+    if filtered_suggestions:
+        selected_suggestion = st.selectbox("Select a suggestion:", filtered_suggestions)
+        st.write("Selected Suggestion:", selected_suggestion)
 
 if __name__ == "__main__":
     main()
